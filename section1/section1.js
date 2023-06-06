f8c480f8867c2fcd6b2dd130b370dda4202aa37a
Sanjeet Kotarya <sanjeetkotarya@gmail.com>
2023-06-06 09:56:18 +0530

    Add files via upload

---
+++ section1/section1.js
@@ -0,0 +1,8 @@
+fetch('section1/section1.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#section1-section");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
