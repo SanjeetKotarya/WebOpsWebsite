1ded8472a84b9f5c7641102676df07508bef2dac
Sanjeet Kotarya <sanjeetkotarya@gmail.com>
2023-06-10 17:42:18 +0530

    Update team.js

--- Team/team.js
+++ Team/team.js
@@ -7,73 +7,27 @@ fetch('Team/team.html')
         oldelem.parentNode.replaceChild(newelem, oldelem);
     })
 
-
-    let input = document.getElementById("input-box");
-let button = document.getElementById("submit-button");
-let showContainer = document.getElementById("show-container");
-let listContainer = document.querySelector(".list");
-
-let date = new Date();
-console.log(date.getTime());
-
-const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
-
-function displayWords(value) {
-  input.value = value;
-  removeElements();
-}
-
-function removeElements() {
-  listContainer.innerHTML = "";
-}
-
-input.addEventListener("keyup", async () => {
-  removeElements();
-  if (input.value.length < 4) {
-    return false;
-  }
-
-  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
-
-  const response = await fetch(url);
-  const jsonData = await response.json();
-
-  jsonData.data["results"].forEach((result) => {
-    let name = result.name;
-    let div = document.createElement("div");
-    div.style.cursor = "pointer";
-    div.classList.add("autocomplete-items");
-    div.setAttribute("onclick", "displayWords('" + name + "')");
-    let word = "<b>" + name.substr(0, input.value.length) + "</b>";
-    word += name.substr(input.value.length);
-    div.innerHTML = `<p class="item">${word}</p>`;
-    listContainer.appendChild(div);
-  });
-});
-
-button.addEventListener(
-  "click",
-  (getRsult = async () => {
-    if (input.value.trim().length < 1) {
-      alert("Input cannot be blank");
+    const scrollElement = document.querySelector(".carousel");
+    const scrollLeft = document.querySelector(".scroll-left");
+    const scrollRight = document.querySelector(".scroll-right");
+    
+    function moveScroll(displacement) {
+      let currentPosition = scrollElement.scrollLeft;
+      scrollElement.scrollTo(currentPosition + displacement, 0);
     }
-    showContainer.innerHTML = "";
-    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
-
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    jsonData.data["results"].forEach((element) => {
-      showContainer.innerHTML = `<div class="card-container">
-        <div class="container-character-image">
-        <img src="${
-          element.thumbnail["path"] + "." + element.thumbnail["extension"]
-        }"/></div>
-        <div class="character-name">${element.name}</div>
-        <div class="character-description">${element.description}</div>
-        </div>`;
+    
+    scrollLeft.addEventListener("click", () => {
+      moveScroll(-200);
     });
-  })
-);
-window.onload = () => {
-  getRsult();
-};
+    
+    scrollRight.addEventListener("click", () => {
+      moveScroll(200);
+    });
+    
+    onload = () => {
+      console.log(scrollElement.style);
+      if (scrollElement.style.display) {
+        scrollLeft.style.display = "none";
+      }
+    };
+    
