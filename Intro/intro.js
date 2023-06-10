85e28e98b3e53148f1c306476d69a4b690985b29
Sanjeet Kotarya <sanjeetkotarya@gmail.com>
2023-06-10 12:07:04 +0530

    Delete Intro directory

--- Intro/intro.js
+++
@@ -1,8 +0,0 @@
-fetch('Intro/intro.html')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#intro-section");
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem, oldelem);
-    })
