f481ccfec014f44396ea089b4345f38203831e38
Sanjeet Kotarya <sanjeetkotarya@gmail.com>
2023-07-09 10:42:21 +0530

    Update projects.js

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,4 +1,4 @@
-fetch('Projects/projects.html')
+fetch('Projects/project.html')
     .then(res => res.text())
     .then(text => {
         let oldelem = document.querySelector("script#project-section");
@@ -6,22 +6,3 @@ fetch('Projects/projects.html')
         newelem.innerHTML = text;
         oldelem.parentNode.replaceChild(newelem, oldelem);
     })
-
-    $('.filters ul li').click(function() {
-        // $('.filters ul li').removeClass('active');
-        // $(this).addClass('active');
-        $('.filters ul li').removeClass('after-click');
-        $(this).addClass('after-click');
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-          filter: data,
-        });
-      });
-      
-      var $grid = $('.grid').isotope({
-        itemSelector: '.all',
-        percentPosition: true,
-        masonry: {
-          columnWidth: '.all',
-        },
-      });
